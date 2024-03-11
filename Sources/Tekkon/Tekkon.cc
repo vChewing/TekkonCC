3b7dc43716e147fc5d6072e957bed4c973edf079
ShikiSuen <shikisuen@outlook.com>
2024-03-11 15:19:24 +0800

    Use SPM as the build manager; etc. (#44)

---
+++ Sources/Tekkon/Tekkon.cc
@@ -0,0 +1,13 @@
+// (c) 2022 and onwards The vChewing Project (MIT-NTL License).
+// ====================
+// This code is released under the MIT license (SPDX-License-Identifier: MIT)
+// ... with NTL restriction stating that:
+// No trademark license is granted to use the trade names, trademarks, service
+// marks, or product names of Contributor, except as required to fulfill notice
+// requirements defined in MIT License.
+
+#include "./include/Tekkon.hh"
+
+namespace Tekkon {};
+
+int main(int argc, const char* argv[]) { return 0; }
